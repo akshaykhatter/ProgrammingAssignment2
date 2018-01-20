c3638b3eaf0e309591e3552be6f9d2bdc7b4400c
akshaykhatter <akshaykhatter97@gmail.com>
2018-01-21 04:11:13 +0530

    functions completed and tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cache a matrix to save on time by spending more memory
 
-## Write a short comment describing this function
+
+## performs 4 simple functions as follows
+## here <<- operator checks the value in the parent env
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  setmat <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  getmat <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(setmat = setmat, getmat = getmat,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## computes the inv iff it is null before
 
 cacheSolve <- function(x, ...) {
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$getmat()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
